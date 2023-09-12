74b93158d3549c3bbd63c927f1139c23b49579a8
pietrolupi <pietro.lupi92@gmail.com>
2023-09-12 15:25:56 +0200

    [edit]:js

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,39 @@
 const mailUser  = prompt('Inserisci la tua mail!');
 
 const mailList = ['falson.DeFalsoni@gmail.com', 'mail.assolutamente.reale@gmail.com', 'mario.rossi@gmail.com', 'lorem.ipsum@gmail.com'];
-
-for(let i = 0 ; i<mailList.length; i++){
+let mailFound = false;
+  
+/* 
+for(let i = 0 ; i < mailList.length; i++){
   let mailCheck = mailList[i];
 
   if(mailCheck === mailUser){
-    console.log('DAGLIEEEE')
+    mailFound = true;
+    console.log('DAGLIEEEE');
+    
+    if(mailFound = false){
+      console.log('RIPROVA');
+    }else if(mailFound = true){
+      console.log('DAGLIEEE');
+    }
+
+     
   };
+};
+ */
+
+
+for(let i = 0 ; i < mailList.length; i++){
+  let mailCheck = mailList[i];
+  
+  if(mailCheck === mailUser){
+    console.log('DAGLIEEE');
+    document.getElementById('test').innerHTML = 'HAI VINTO';
+  }else{
+    console.log('RIPROVA');
+    document.getElementById('test').innerHTML = 'HAI PERSO';
+  };
+
+
+
 }
