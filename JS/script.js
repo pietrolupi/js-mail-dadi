63469a2ecaf9276fdbd739f90d3f2935d4e73e37
pietrolupi <pietro.lupi92@gmail.com>
2023-09-12 17:11:33 +0200

    [edit]: html, js

--- JS/script.js
+++ JS/script.js
@@ -1,22 +1,30 @@
 
 ////// MAIL ///////////////////
-const mailUser  = prompt('Inserisci la tua mail!');
 
+
+const mailUser  = document.getElementById('form-mail');
 const mailList = ['falson.DeFalsoni@gmail.com', 'mail.assolutamente.reale@gmail.com', 'mario.rossi@gmail.com', 'lorem.ipsum@gmail.com', 'test'];
+const btnMail = document.querySelector('.btn-mail');
+
+btnMail.addEventListener('click', function(){
+  
+  for(let i = 0 ; i < mailList.length; i++){
+  
+    let mailCheck = mailList[i];
+  
+  if(mailCheck === mailUser.value){
+    
+    document.getElementById('test').innerHTML = 'MAIL IN LISTA :)';
+      }else if(mailCheck !== mailUser.value){
+    
+    document.getElementById('test').innerHTML = 'MAIL NON IN LISTA :(';
+  };
+  }
 
 
-for(let i = 0 ; i < mailList.length; i++){
-  
-  let mailCheck = mailList[i];
+})
 
-if(mailCheck === mailUser){
-  
-  document.getElementById('test').innerHTML = 'MAIL IN LISTA :)';
-    }else if(mailCheck !== mailUser){
-  
-  document.getElementById('test').innerHTML = 'MAIL NON IN LISTA :(';
-};
-}
+
 
 ////////// DADI ///////////////////////
 
@@ -27,15 +35,27 @@ roll.addEventListener('click', function(){
 
   const diceUser = Math.floor(Math.random() * (7 - 1) + 1);
   const diceComputer = Math.floor(Math.random() * (7 - 1) + 1);
+  const result = document.querySelector('.show-winner > span');
   
   if(diceComputer > diceUser){
-    console.log('hai persooo' + diceComputer + diceUser)
+    result.innerHTML = 'HAI PERSO :('
   }else if(diceComputer < diceUser){
-    console.log('HAI VINTO!'+ diceComputer + diceUser)
-  }else if(diceComputer === diceUser){
-    console.log('PAREGGIO!'+ diceComputer + diceUser)
+    result.innerHTML = 'HAI VINTO :)'
+  }else{
+    result.innerHTML = 'PAREGGIO, RITENTA!'
   };
   
+  //////////stampe in HTML
+  const showRoll = document.querySelector('.show-roll');
+  const showRoll2 = document.querySelector('.show-roll-2');
+  const showWinner = document.querySelector('.show-winner');
+  showRoll.classList.remove('d-none');
+  showRoll2.classList.remove('d-none');
+  showWinner.classList.remove('d-none');
+  document.querySelector('.show-roll > span').innerHTML = diceUser;
+  document.querySelector('.show-roll-2 > span').innerHTML = diceComputer;
+ 
+
 });
 
 
