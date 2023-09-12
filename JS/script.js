d2173fa36805871e7398d250ddc87789a393d5e7
pietrolupi <pietro.lupi92@gmail.com>
2023-09-12 15:56:38 +0200

    [edit]:js

--- JS/script.js
+++ JS/script.js
@@ -1,39 +1,20 @@
 const mailUser  = prompt('Inserisci la tua mail!');
 
-const mailList = ['falson.DeFalsoni@gmail.com', 'mail.assolutamente.reale@gmail.com', 'mario.rossi@gmail.com', 'lorem.ipsum@gmail.com'];
-let mailFound = false;
-  
-/* 
+const mailList = ['falson.DeFalsoni@gmail.com', 'mail.assolutamente.reale@gmail.com', 'mario.rossi@gmail.com', 'lorem.ipsum@gmail.com', 'test'];
+
+
 for(let i = 0 ; i < mailList.length; i++){
+  
   let mailCheck = mailList[i];
 
-  if(mailCheck === mailUser){
-    mailFound = true;
-    console.log('DAGLIEEEE');
-    
-    if(mailFound = false){
-      console.log('RIPROVA');
-    }else if(mailFound = true){
-      console.log('DAGLIEEE');
-    }
-
-     
-  };
+if(mailCheck === mailUser){
+  
+  document.getElementById('test').innerHTML = 'MAIL IN LISTA :)';
+    }else if(mailCheck !== mailUser){
+  
+  document.getElementById('test').innerHTML = 'MAIL NON IN LISTA :(';
 };
- */
-
-
-for(let i = 0 ; i < mailList.length; i++){
-  let mailCheck = mailList[i];
-  
-  if(mailCheck === mailUser){
-    console.log('DAGLIEEE');
-    document.getElementById('test').innerHTML = 'HAI VINTO';
-  }else{
-    console.log('RIPROVA');
-    document.getElementById('test').innerHTML = 'HAI PERSO';
-  };
-
-
-
 }
+
+
+
