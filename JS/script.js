e6cadd5def77f6f302b16419925d28d148de2521
pietrolupi <pietro.lupi92@gmail.com>
2023-09-12 16:17:29 +0200

    [edit]:js

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,5 @@
+
+////// MAIL ///////////////////
 const mailUser  = prompt('Inserisci la tua mail!');
 
 const mailList = ['falson.DeFalsoni@gmail.com', 'mail.assolutamente.reale@gmail.com', 'mario.rossi@gmail.com', 'lorem.ipsum@gmail.com', 'test'];
@@ -16,5 +18,24 @@ if(mailCheck === mailUser){
 };
 }
 
+////////// DADI ///////////////////////
+
+
+const roll = document.getElementById('roll');
+
+roll.addEventListener('click', function(){
+
+  const diceUser = Math.floor(Math.random() * (7 - 1) + 1);
+  const diceComputer = Math.floor(Math.random() * (7 - 1) + 1);
+  
+  if(diceComputer > diceUser){
+    console.log('hai persooo' + diceComputer + diceUser)
+  }else if(diceComputer < diceUser){
+    console.log('HAI VINTO!'+ diceComputer + diceUser)
+  }else if(diceComputer === diceUser){
+    console.log('PAREGGIO!'+ diceComputer + diceUser)
+  };
+  
+});
 
 
